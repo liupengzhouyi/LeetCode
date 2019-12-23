ebce007605d97c1f133c1a2361a5e56ecd138e26
liupeng <liupeng.0@outlook.com>
2019-12-23 23:20:39 +0800

    动态规划 V 0.0.4

--- main.cpp
+++ main.cpp
@@ -1,44 +1,67 @@
 #include <iostream>
 #include <vector>
 #include "day01/paly.h"
+#include <math.h>
 
 using namespace std;
+
+void paly(int number) {
+
+
+
+
+
+}
+
 
 int main() {
     vector<int> obj;
     vector<int> object;
     vector<int> temp;
 
+    int target = 102;
     // push_back(elem)在数组最后添加数据
-    for(int i=0;i<5;i++)
+    /*for(int i=0;i<5;i++)
     {
-        obj.push_back(i+1);
-    }
+        obj.push_back(i+3);
+    }*/
+    obj.push_back(9);
+    obj.push_back(3);
+    obj.push_back(83);
+    obj.push_back(5);
+    obj.push_back(7);
+    obj.push_back(53);
+
 
     object.push_back(0);
 
     for(int i=0;i<obj.size();i++)//size()容器中实际数据个数
     {
-        object.push_back(obj[i]);
-        for (int l = 0; l < object.size(); l++) {
-            cout << object[l] << " ";
-        }
-        cout << endl;
-
-        for (int j = 0; j < object.size(); j++) {
-            int t = obj[i] + object[j];
-            cout << "[" << t << "]";
-            temp.push_back(t);
+        cout << "当前元素：" << obj[i] << " - ";
+        if(i == 0) {
+            object.push_back(obj[i]);
+        } else {
+            for (int j = 0; j < object.size(); j++) {
+                int t = obj[i] + object[j];
+                temp.push_back(t);
+            }
         }
 
         for (int k = 0; k < temp.size(); k++)//size()容器中实际数据个数
         {
             object.push_back(temp[k]);
-            // cout << temp[k] << " ";
         }
-
+        // 清空本次产生的数据
         temp.clear();
-
+        cout << endl;
+        for (int l = 0; l < object.size(); l++) {
+            cout << l+1 << " ";
+        }
+        cout << endl;
+        for (int l = 0; l < object.size(); l++) {
+            cout << object[l] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
