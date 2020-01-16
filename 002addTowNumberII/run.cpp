b2c33610e9908c897e92fcc313e5e4666507db5d
liupeng <liupeng.0@outlook.com>
2020-01-16 23:26:05 +0800

    Begin V 1.0.0

---
+++ 002addTowNumberII/run.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 刘鹏 on 2020/1/16.
+//
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include "AddTowNumberII.h"
+
+using namespace std;
+
+
+/*
+
+int main() {
+    AddTowNumberII addTowNumberIi;
+    addTowNumberIi.init();
+    std::vector<int> vector1 = addTowNumberIi.twoSumII(addTowNumberIi.getVector(), 9);
+    for (auto number : vector1) {
+        std::cout << number << std::endl;
+    }
+    return 0;
+}
+*/
--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <vector>
-#include "001addTwoNumber/AddTownumber.h"
-#include "aotu/test.h"
-#include "002addTowNumberII/AddTowNumberII.h"
-#include <math.h>
-
 using namespace std;
 
-
-
 int main() {
-    AddTowNumberII addTowNumberIi;
-    addTowNumberIi.init();
-    std::vector<int> vector1 = addTowNumberIi.twoSumII(addTowNumberIi.getVector(), 9);
-    for (auto number : vector1) {
-        std::cout << number << std::endl;
-    }
+
     return 0;
 }
 
