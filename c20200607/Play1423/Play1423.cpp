f5cda3ee3127d23c4a15ce07a77a11af9d13253d
liupeng <liupeng.0@outlook.com>
2020-06-07 18:08:24 +0800

    可获得的最大点数 V 1.2.4

---
+++ c20200607/Play1423/Play1423.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 刘鹏 on 2020/6/7.
+//
+
+#include "Play1423.h"
+
+int Play1423::maxScore(std::vector<int> &cardPoints, int k) {
+    auto sum = 0, max = 0;
+    if (k >= cardPoints.size()) {
+        for (auto item : cardPoints) {
+            sum = sum + item;
+        }
+    } else {
+        for (auto i = 0; i < k; i++) {
+            sum = sum + cardPoints[i];
+        }
+        max = sum;
+        auto index = 1;
+        for (auto i = k-1; i >= 0; i--) {
+            std::cout << cardPoints[i] << std::endl;
+            sum = sum - cardPoints[i] + cardPoints[cardPoints.size()-index];
+            index = index + 1;
+            max = max < sum ? sum : max;
+        }
+        sum = max;
+    }
+    return sum;
+}
---
+++ c20200607/Play1423/Play1423.h
@@ -0,0 +1,16 @@
+//
+// Created by 刘鹏 on 2020/6/7.
+//
+
+#ifndef LEETCODE_PLAY1423_H
+#define LEETCODE_PLAY1423_H
+#include <iostream>
+#include <vector>
+
+class Play1423 {
+public:
+    int maxScore(std::vector<int> &cardPoints, int k);
+};
+
+
+#endif //LEETCODE_PLAY1423_H
