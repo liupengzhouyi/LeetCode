2a858824a3abc520bcc42cee98a06f4d93737abb
liupeng <liupeng.0@outlook.com>
2020-06-14 21:58:25 +0800

    实现 strStr() V 1.2.11

---
+++ c20200614/Paly28/Paly28.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 刘鹏 on 2020/6/14.
+//
+
+#include "Paly28.h"
+
+int Paly28::strStr(std::string haystack, std::string needle) {
+    if(!needle.size()) return 0;
+    if(!haystack.size()) return -1;
+    //先构造pattern
+    int j = -1, i = 0;//j在后面，i在前面
+    vector<int> b(needle.size() + 1);
+    b[i] = j;
+    while(i < needle.size())
+    {
+        while(j >= 0 && needle[i] != needle[j]) j = b[j];
+        i++, j++;
+        b[i] = j;
+    }
+
+    j = 0, i = 0; //j这回是text的， i是pattern的
+    while(j < haystack.size())
+    {
+        while(i >= 0 && needle[i] != haystack[j]) i = b[i];
+        i++, j++;
+        if(i == needle.size())
+        {
+            return j - needle.size();
+        }
+    }
+    return -1;
+}
---
+++ c20200614/Paly28/Paly28.h
@@ -0,0 +1,18 @@
+//
+// Created by 刘鹏 on 2020/6/14.
+//
+
+#ifndef LEETCODE_PALY28_H
+#define LEETCODE_PALY28_H
+
+#include <iostream>
+#include <string>
+
+
+class Paly28 {
+public:
+    int strStr(std::string haystack, std::string needle);
+};
+
+
+#endif //LEETCODE_PALY28_H
